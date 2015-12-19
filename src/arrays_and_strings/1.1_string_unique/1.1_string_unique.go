9b8b6119abf6809c7d8bfe20b97938f5aa956a15
Chris White <chris@cwhite.me>
2015-12-19 23:46:05 +0000

    Initial commit - solution for problem 1.1

---
+++ src/arrays_and_strings/1.1_string_unique/1.1_string_unique.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  words := []string {"abcde", "test", "ぁあぃぎじ", "せぬがだぬ", ""}
+
+  for _, word := range words {
+    fmt.Printf("Is \"%s\" unique? %t\n", word, unique(word))
+  }
+}
+
+func unique(input string) bool {
+  seen := make(map[rune]bool)
+
+  for _, c := range input {
+    // If we've seen this character before, the string is not unique.
+    if _, ok := seen[c]; ok {
+      return false
+    }
+
+    seen[c] = true
+  }
+
+  return true
+}
---
+++ src/arrays_and_strings/1.1_string_unique/1.1_string_unique_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestUnique(t *testing.T) {
+  var tests = []struct {
+    input string
+    unique bool
+  }{
+    {"abcde", true},
+    {"test", false},
+    {"ぁあぃぎじ", true},
+    {"せぬがだぬ", false},
+    {"The quick brown fox jumps over the lazy dog", false},
+    {"", true},
+  }
+
+  for _, c := range tests {
+    got := unique(c.input)
+
+    if got != c.unique {
+      t.Errorf("unique(%q) == %q, want %q", c.input, got, c.unique)
+    }
+  }
+}
