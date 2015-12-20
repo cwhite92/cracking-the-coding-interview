4b3a4ea7a55d4254b11580fc296c56469a99895f
Chris White <chris@cwhite.me>
2015-12-20 02:02:08 +0000

    1.2 Reverse string test

---
+++ src/arrays_and_strings/1.2_string_reverse/1.2_string_reverse_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+  var tests = []struct {
+    input string
+    output string
+  }{
+    {"abcde", "edcba"},
+    {"hello world", "dlrow olleh"},
+    {"ぁあぃぎじ", "じぎぃあぁ"},
+    {"", ""},
+    {"()[]", "][)("},
+  }
+
+  for _, c := range tests {
+    got := reverse(c.input)
+
+    if got != c.output {
+      t.Errorf("reverse(%q) == %q, want %q", c.input, got, c.output)
+    }
+  }
+}
