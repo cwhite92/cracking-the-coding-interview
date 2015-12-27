731f7aa89127e2fb67aecbaa577fc1b0648e4d6c
Chris White <chris@cwhite.me>
2015-12-27 22:47:47 +0000

    gofmt everything

--- src/arrays_and_strings/1.2_string_reverse/1.2_string_reverse_test.go
+++ src/arrays_and_strings/1.2_string_reverse/1.2_string_reverse_test.go
@@ -3,22 +3,22 @@ package main
 import "testing"
 
 func TestReverse(t *testing.T) {
-  var tests = []struct {
-    input string
-    output string
-  }{
-    {"abcde", "edcba"},
-    {"hello world", "dlrow olleh"},
-    {"ぁあぃぎじ", "じぎぃあぁ"},
-    {"", ""},
-    {"()[]", "][)("},
-  }
+	var tests = []struct {
+		input  string
+		output string
+	}{
+		{"abcde", "edcba"},
+		{"hello world", "dlrow olleh"},
+		{"ぁあぃぎじ", "じぎぃあぁ"},
+		{"", ""},
+		{"()[]", "][)("},
+	}
 
-  for _, c := range tests {
-    got := reverse(c.input)
+	for _, c := range tests {
+		got := reverse(c.input)
 
-    if got != c.output {
-      t.Errorf("reverse(%q) == %q, want %q", c.input, got, c.output)
-    }
-  }
+		if got != c.output {
+			t.Errorf("reverse(%q) == %q, want %q", c.input, got, c.output)
+		}
+	}
 }
---
+++ src/arrays_and_strings/1.3_string_permutation/1.3_string_permutation_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestPermutation(t *testing.T) {
+	var tests = []struct {
+		str1   string
+		str2   string
+		output bool
+	}{
+		{"the quick brown fox", "het uqkic wborn ofx", true},
+		{"ぁあぃぎじ", "じぎぃあぁ", true},
+		{"()[]", "][)(!", false},
+		{"ぁあぃぎじ", "じぎぃあぁ!", true},
+	}
+
+	for _, test := range tests {
+		got := permutation(test.str1, test.str2)
+
+		if got != test.output {
+			t.Errorf("permutation(%q, %q) == %t, want %t", test.str1, test.str2, got, test.output)
+		}
+	}
+}
